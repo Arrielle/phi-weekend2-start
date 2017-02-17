0d8e9068331e7bc8d9b2c58b90292bd6ff8c661b
Arrielle <arrielle@Arrielles-MacBook-Pro.local>
2017-02-17 17:24:37 -0600

    Have basic css setup, figuring out how to add people to dom based on arrayi

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,7 +6,50 @@ $(document).ready(function(){
       url: "/data",
       success: function(data){
         // yay! we have data!
-        console.log('returned data from server: ', data);
+        // console.log('returned data from server: ', data);
+        // console.log('WHAT IS UP');
+        for (var i = 0; i < data.phirephiters.length; i++) {
+        console.log(data.phirephiters[i].name);
+        }
+
       }
-    });
+    });//end ajax
+
+    //append ONE persons info at DOM load
+    function addingEveryoneWillyNilly(phirephiter) {
+    $('#sliderContainer').append('<div class ="studentContainer">This is a student container. I need to do something with a for loop.</div>');
+} //ends adding willy nilly function
+
 });
+
+//images need to be wrapped with div tags
+
+// On the DOM should be:
+//
+// One person's information
+// A series of 18 (or the number of people in the data array) index points with the first person's
+// index highlighted or called out in style differently than the others.
+// A 'Next' button and a 'Previous' button
+// Clicking on the Next button should navigate to the next person, clicking on the Previous button
+// should navigate to the previous person. The highlighted index point should update also as you click through to other people.
+// Person Display
+//
+// When a person is displayed, show their name, their Github link, and their piece of shoutout feedback.
+// Only one person should be showcased at any given time.
+//
+// You will need to combine everything you learned this week to accomplish this task, and each of the challenges
+// you have completed this week play a part in this task.
+//
+// Working Example
+//
+// Here is a similar example from Zeta so you can see the functionality. It's really ugly, however. The code is
+// also minified (no cheating!):
+//
+// https://polar-ravine-37299.herokuapp.com/
+//
+// HARD MODE
+// Include a fade out and fade in animation in-between transitioning people.
+//
+// PRO MODE
+// Include a timer that moves to the next person if the user is not clicking on next or prev. If the user clicks on
+// next or prev, the timer should be reset. The timer should transition between people every 10 seconds.
